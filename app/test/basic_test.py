726a153c36c9c76c75077f42bee9ec6029a42405
goller <alegoller@gmail.com>
2023-04-20 22:49:54 +0200

    Test for pipeline

---
+++ app/test/basic_test.py
@@ -0,0 +1,17 @@
+"""Temp test"""
+
+import unittest
+
+
+class MyTest(unittest.TestCase):
+    """Create class for Mytest"""
+    def test_addition(self)->None:
+        """test"""
+        self.assertEqual(2 + 2, 4)
+    
+    def test_subtraction(self)->None:
+        """test"""
+        self.assertEqual(5 - 3, 2)
+
+if __name__ == '__main__':
+    unittest.main()
