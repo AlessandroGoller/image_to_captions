46004e4ac99e14c20bb6985dbbd55565f7821645
Alessandro Goller <45425874+AlessandroGoller@users.noreply.github.com>
2023-04-26 23:28:43 +0200

    Create config.py

---
+++ app/config/config.py
@@ -0,0 +1,22 @@
+"""Module with all config"""
+
+import os
+from typing import Optional
+
+from pydantic import BaseSettings
+
+
+class Config(BaseSettings):
+    """Class with the config"""
+    # Database config variables
+    SQLALCHEMY_DATABASE_URI:str = (
+        os.getenv("SQLALCHEMY_DATABASE_URI")
+        or
+        "sqlite:///./sql_app.db"
+    )
+
+    LANGUAGE:str = os.getenv("LANGUAGE") or "italian"
+    PORT:str = os.getenv("PORT") or "5000"
+    HUGGINGFACEHUB_API_TOKEN:Optional[str] = os.getenv("HUGGINGFACEHUB_API_TOKEN") or None
+    OPENAI_API_TOKEN:Optional[str] = os.getenv("OPENAI_API_TOKEN") or None
+    SERPAPI_API_KEY:Optional[str] = os.getenv("SERPAPI_API_KEY") or None
