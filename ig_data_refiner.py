fa6bd0779581bb81799826a3c893f6d1640f7bec
Riccardo <riccardo.ricci-1@unitn.it>
2023-04-27 11:54:02 +0200

    Modified requirements, working on IG scraping

---
+++ ig_data_refiner.py
@@ -0,0 +1,7 @@
+# Instagram data are downloaded in this format
+# post, datetime, mediaid, profile, caption, location
+
+# The aim of this code is to get a csv file in the format of the following example:
+# post, datetime, mediaid, profile, caption, location
+# And convert it into a json file with the following fields:
+# post, datetime, mediaid, profile, caption, location, hashtags, tagged_users, 
