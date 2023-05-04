a7696ca7fb68c8c795a9386e68e256ca39028d12
Riccardo <riccardo.ricci-1@unitn.it>
2023-05-04 14:40:59 +0200

    Fixed all pylint suggestions

--- app/services/ig_scraping.py
+++ app/services/ig_scraping.py
@@ -1,7 +1,11 @@
+"""
+Module providing functions for scraping information from instagram accounts.
+"""
 import csv
 import json
 from datetime import datetime
 from itertools import dropwhile, takewhile
+from typing import Optional
 
 import instaloader
 from tqdm import tqdm
@@ -49,29 +53,30 @@ class GetInstagramProfile:
         """
         self.L.login(input("input your username: "), input("input your password: "))
         profile = instaloader.Profile.from_username(self.L.context, user_name)
-        file = open("follower_names.txt", "a+")
-        for followee in profile.get_followers():
-            username = followee.username
-            file.write(username + "\n")
-            print(username)
+        with open("follower_names.txt", "a+", encoding="utf-8") as file:
+            for follower in profile.get_followers():
+                username = follower.username
+                file.write(username + "\n")
+                print(username)
 
-    def get_users_followings(self, user_name:str) -> None:
+    def get_users_followings(self, username:str) -> None:
         """
         Function to get a profile's followings.
         Note: login required to get a profile's followings.
         """
         self.L.login(input("input your username: "), input("input your password: "))
-        profile = instaloader.Profile.from_username(self.L.context, user_name)
-        file = open("following_names.txt", "a+")
-        for followee in profile.get_followees():
-            username = followee.username
-            file.write(username + "\n")
-            print(username)
+        profile = instaloader.Profile.from_username(self.L.context, username)
+        with open("following_names.txt", "a+", encoding="utf-8") as file:
+            for followee in profile.get_followees():
+                username = followee.username
+                file.write(username + "\n")
+                print(username)
 
     def get_post_comments(self, username:str) -> None:
         """
         Function to get a post's comments and print them at screen.
         """
+
         posts = instaloader.Profile.from_username(self.L.context, username).get_posts()
         for post in posts:
             for comment in post.get_comments():
@@ -85,6 +90,7 @@ class GetInstagramProfile:
         """
         Function to get info from every post of a user and store them in a csv file.
         """
+
         with open(username + ".csv", "w", newline="", encoding="utf-8") as file:
             writer = csv.writer(file)
             posts = instaloader.Profile.from_username(
@@ -144,7 +150,7 @@ class GetInstagramProfile:
             "Downloading last " + str(last_n_posts) + " posts from " + username + "..."
         )
         for _i in tqdm(range(last_n_posts)):
-            post = posts.__next__()
+            post = next(posts)
             shortcode = post.mediaid_to_shortcode(post.mediaid)
             data[shortcode] = {}
             # Store the post info
@@ -163,7 +169,7 @@ class GetInstagramProfile:
 
         return data
 
-def load_post_captions_from_json(json_file: str, shortcodes: list = None) -> list:
+def load_post_captions_from_json(json_file: str, shortcodes: Optional[list] = None) -> list:
     """
     Function to load post captions from a json file. The post loaded are the one specified by the shortcodes list.
     This to allow compatibility with eventual search functions on the specific post.
@@ -185,17 +191,23 @@ def load_post_captions_from_json(json_file: str, shortcodes: list = None) -> lis
 
 
 if __name__ == "__main__":
-    username = "montura_official"
-    client = GetInstagramProfile()
-    data = client.get_post_info_json(username,last_n_posts=2)
-
     from app.crud.company import get_company_by_user_id
     from app.crud.instagram import insert_data_to_db
     from app.crud.user import get_user_by_email
 
-    user_id = get_user_by_email(email="test@gmail.com").user_id
-    company_id = get_company_by_user_id(user_id=user_id).id_company
-    if insert_data_to_db(data=data,user_id=user_id,company_id=company_id):
+    username_ = "montura_official"
+    client = GetInstagramProfile()
+    data_ = client.get_post_info_json(username_,last_n_posts=2)
+
+    user = get_user_by_email(email="test@gmail.com")
+    if user is not None:
+        user_id = user.user_id
+
+    company = get_company_by_user_id(user_id=user_id)
+    if company is not None:
+        company_id = company.company_id
+
+    if insert_data_to_db(data=data_,user_id=user_id,company_id=company_id):
         print("Aggiunto correttamente nel db")
     else:
         print("Errorr")
