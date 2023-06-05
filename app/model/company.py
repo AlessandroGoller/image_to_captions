3e73222efd3ec9c816dd62b7a57452df5ba2e80a
Alessandro Goller <alessandro.goller@kwantis.com>
2023-06-05 12:09:55 +0200

    feat: remove_tagged_users
    fix: remove unique name in t_company db

--- app/model/company.py
+++ app/model/company.py
@@ -11,7 +11,7 @@ class Company(database):
     __tablename__ = "t_company"
 
     id_company = Column("id_t_company", Integer, nullable=False, primary_key=True)
-    name = Column("name", String, nullable=False, unique=True)
+    name = Column("name", String, nullable=False)
     id_user = Column(Integer, ForeignKey("t_user.id_t_user"), nullable=False)
     url_instagram = Column(String, nullable=True)
     language = Column(String, nullable=True, default= "Auto")
