cda4106d372d3999148519982cc1d887acdbbd1e
Alessandro Goller <alessandro.goller@kwantis.com>
2023-06-21 18:07:20 +0200

    cast int

--- app/crud/telegram.py
+++ app/crud/telegram.py
@@ -15,22 +15,22 @@ logger = configure_logger()
 def get_telegram_by_user_id(id_user: int) -> Optional[Telegram]:
     """Return the Telegram from id_user"""
     db: Session = next(get_db())
-    return db.query(Telegram).filter(Telegram.id_user == id_user).first()  # type: ignore
+    return db.query(Telegram).filter(Telegram.id_user == int(id_user)).first()  # type: ignore
 
 def get_telegram_by_user_tele_id(id_user_tele: int) -> Optional[Telegram]:
     """Return the Telegram from id_user"""
     db: Session = next(get_db())
-    return db.query(Telegram).filter(Telegram.id_user_telegram == id_user_tele).first()  # type: ignore
+    return db.query(Telegram).filter(Telegram.id_user_telegram == int(id_user_tele)).first()  # type: ignore
 
 def get_telegram_by_chat_id(id_chat: int) -> Optional[Telegram]:
     """Return the Telegram from id_user"""
     db: Session = next(get_db())
-    return db.query(Telegram).filter(Telegram.id_chat == id_chat).first()  # type: ignore
+    return db.query(Telegram).filter(Telegram.id_chat == int(id_chat)).first()  # type: ignore
 
 def get_telegram_by_id(id_telegram: int) -> Optional[Telegram]:
     """Return the Telegram from id_telegram"""
     db: Session = next(get_db())
-    return db.query(Telegram).filter(Telegram.id_telegram == id_telegram).first()  # type: ignore
+    return db.query(Telegram).filter(Telegram.id_telegram == int(id_telegram)).first()  # type: ignore
 
 def create_telegram(telegram: TelegramCreate) -> Telegram:
     """Creation a telegram, in input the schema of telegram create and return the telegram"""
@@ -53,7 +53,7 @@ def update_last_access(telegram: Telegram) -> None:
 def delete_telegram_from_id_chat(id_chat: int) -> dict[str, bool]:
     """Permit to delete a telegram from an id_chat"""
     db: Session = next(get_db())
-    telegram:Telegram = db.query(Telegram).filter_by(id_chat=id_chat).first()
+    telegram:Telegram = db.query(Telegram).filter_by(id_chat=int(id_chat)).first()
     db.delete(telegram)
     db.commit()
     return {"ok": True}
@@ -69,7 +69,7 @@ def delete_all_telegram(id_user:int) -> None:
     """ Delete all telegram account connected to a id_user """
     db: Session = next(get_db())
     # Get all Telegram accounts connected to the user
-    telegrams: list[Telegram] = db.query(Telegram).filter_by(id_user=id_user).all()
+    telegrams: list[Telegram] = db.query(Telegram).filter_by(id_user=int(id_user)).all()
     # Delete each Telegram account
     for telegram in telegrams:
         try:
