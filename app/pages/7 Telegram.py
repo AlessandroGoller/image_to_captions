cb71d3adaba7ccddad275430d52456c4786d3ad4
Alessandro Goller <alessandro.goller@kwantis.com>
2023-06-21 17:45:47 +0200

    update telegram page

--- app/pages/7 Telegram.py
+++ app/pages/7 Telegram.py
@@ -4,17 +4,18 @@
 import streamlit as st
 from streamlit_extras.switch_page_button import switch_page
 
-from app.crud.user import get_id_user_by_email
-from app.services.telegram_bot import get_telegram_url
+from app.crud.user import get_hash_from_email
+from app.dependency import get_settings
 from app.utils.logger import configure_logger
 from app.utils.streamlit_utils.auth import is_logged_in
 
+settings = get_settings()
 logger = configure_logger()
 
 if not is_logged_in(session_state=st.session_state):
     switch_page("login")
 
-id_user: str = get_id_user_by_email(email=st.session_state["email"])
-telegram_url = get_telegram_url(user_id=id_user)
+hash_code: str = get_hash_from_email(email=st.session_state["email"])
+telegram_url = f"https://telegram.me/{settings.TELEGRAM_BOT}?start={hash_code}"
 # use markdown for link
 st.write(telegram_url)
